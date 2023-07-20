a964161ed62ba22655796ad1fda3f61ada9b0887
David <daveman@gmail.com>
2023-07-20 00:13:40 -0700

    complete challenge 5

--- app.js
+++ app.js
@@ -18,8 +18,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-
   res.render("home", {content: homeStartingContent} )
+})
+app.get("/about", function (req, res) {
+  res.render("about", {content: aboutContent} )
+})
+app.get("/contact", function (req, res) {
+  res.render("contact", {content: contactContent} )
 })
 
 app.listen(process.env.PORT || 3000, function() {
