6b0e6eb9f995d12ea168d0f5d848a84c42c24164
David <daveman@gmail.com>
2023-07-20 01:17:02 -0700

    complete challenges 9, 10, and 11

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+let posts = [];
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +20,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {content: homeStartingContent} )
+  res.render("home", { content: homeStartingContent })
+
+  console.log(posts);
+
 })
 app.get("/about", function (req, res) {
   res.render("about", {content: aboutContent} )
@@ -32,7 +37,14 @@ app.get("/compose", function (req, res) {
 })
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.newBlogPost);
+  const post = {
+    title: req.body.newBlogTitle,
+    content: req.body.newBlogPost
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 })
 
 app.listen(process.env.PORT || 3000, function() {
